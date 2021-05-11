693a71da040e47f273fe68b587fafc015c213506
Aravind Balla <bsaaravind@gmail.com>
2021-05-11 11:34:12 +0530

    feat: add workbox wip

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+      // Do not precache images
+      // exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+      // Define runtime caching rules.
+      runtimeCaching: [
+        {
+          // Match any request that ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+          // Apply a cache-first strategy.
+          handler: "CacheFirst",
+
+          options: {
+            // Use a custom cache name.
+            cacheName: "assets",
+
+            // Only cache 10 images.
+            expiration: {
+              maxEntries: 10,
+            },
+          },
+        },
+        {
+          urlPattern: /\.(js|css)$/,
+          // Apply a cache-first strategy.
+          handler: "CacheFirst",
+          options: {
+            cacheName: "assets",
+            expiration: {
+              maxEntries: 10,
+            },
+          },
+        },
+        {
+          urlPattern: /\.html$/,
+          handler: "NetworkFirst",
+
+          options: {
+            cacheName: "assets",
+          },
+        },
+      ],
+    }),
+  ],
+};
