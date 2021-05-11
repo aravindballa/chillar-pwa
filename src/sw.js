81d338de85850ecd1397529b38d3646bf17a9273
Aravind Balla <bsaaravind@gmail.com>
2021-05-11 12:36:37 +0530

    fix: cache html

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 // import { precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { CacheFirst } from "workbox-strategies";
+import { CacheFirst, NetworkFirst } from "workbox-strategies";
 
 // precacheAndRoute(self.__WB_MANIFEST);
 
@@ -8,4 +8,18 @@ registerRoute(
   new RegExp("\\.(png|jpg|jpeg|svg)$"),
   new CacheFirst({ cacheName: "images" })
 );
-registerRoute(new RegExp("\\.js$"), new CacheFirst({ cacheName: "js" }));
+
+registerRoute(
+  ({ request }) =>
+    request.destination === "script" || request.destination === "style",
+  new Cas({
+    cacheName: "static-resources",
+  })
+);
+
+registerRoute(
+  new RegExp("\\.html$"),
+  new NetworkFirst({
+    cacheName: "shell",
+  })
+);
