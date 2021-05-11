d9777264d0300f393c9004d209b314cf4f0c8359
Aravind Balla <bsaaravind@gmail.com>
2021-05-11 12:11:15 +0530

    fix: sw error

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,6 @@
 import { precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
+import { CacheFirst } from "workbox-strategies";
 
 precacheAndRoute(self.__WB_MANIFEST);
 
