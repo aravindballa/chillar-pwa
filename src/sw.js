143548a2a8e64fcd0fc171e78fe180d40a93b6c4
Aravind Balla <bsaaravind@gmail.com>
2021-05-11 12:44:06 +0530

    fix: try nav route

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 // import { precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { CacheFirst, NetworkFirst } from "workbox-strategies";
+import { CacheFirst, NetworkOnly } from "workbox-strategies";
 
 // precacheAndRoute(self.__WB_MANIFEST);
 
@@ -17,9 +17,37 @@ registerRoute(
   })
 );
 
-registerRoute(
-  new RegExp("\\.html$"),
-  new NetworkFirst({
-    cacheName: "shell",
-  })
-);
+// Doesn't seem to work
+// registerRoute(
+//   new RegExp("\\.html$"),
+//   new NetworkFirst({
+//     cacheName: "shell",
+//   })
+// );
+
+const CACHE_NAME = "html";
+const FALLBACK_HTML_URL = "/index.html"; // Fallback is also main page
+
+self.addEventListener("install", async (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.add(FALLBACK_HTML_URL))
+  );
+});
+
+navigationPreload.enable();
+
+const networkOnly = new NetworkOnly();
+const navigationHandler = async (params) => {
+  try {
+    // Attempt a network request.
+    return await networkOnly.handle(params);
+  } catch (error) {
+    // If it fails, return the cached HTML.
+    return caches.match(FALLBACK_HTML_URL, {
+      cacheName: CACHE_NAME,
+    });
+  }
+};
+
+// Register this strategy to handle all navigations.
+registerRoute(new NavigationRoute(navigationHandler));
